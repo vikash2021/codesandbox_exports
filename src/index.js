3194e898671c071485b0060744de6d9278ab4c0e
Vikash Kumar <kumarv4624@gmail.com>
2023-07-08 14:36:29 +0530

    Added separate folder for js core concepts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-let multiply = function (x, y) {
-  console.log(x * y);
-};
+import currying from "./CoreJsConcepts/currying";
 
-let add = function (x) {
-  return function (y) {
-    console.log(x + y);
-  };
-};
+currying();
 
-let multiplyByTwo = multiply.bind(this, 2);
-multiplyByTwo(3);
-
-let addHere = add(5);
-addHere(8);
+//vikash
